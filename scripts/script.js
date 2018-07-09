4a713fe8131b5d2adea03ec0280aa93956a42629
Amandeep Kaur <A01063289@ad.bcit.ca>
2018-07-09 16:02:53 -0700

    Create mobile styles

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// Nav Show hide
+const body = document.body;
+const btn = document.getElementById('btn-hamburger');
+
+btn.addEventListener('click', function(){
+
+    body.classList.toggle('show');
+
+})
